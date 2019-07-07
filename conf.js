31aa08b0a9599341dce6b4d682c9cda959ec6c2d
xulang726 <xulang726@users.noreply.github.com>
2019-07-07 20:47:03 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -8,20 +8,20 @@ jsproxy_config({
       label: '轻量云-香港',
       lines: [
         // 多条线路，负载均衡系统会从其中选一条
-        'node-aliyun-hk-0.etherdream.com:8443',
-        'node-aliyun-hk-1.etherdream.com:8443'
+        //'node-aliyun-hk-0.etherdream.com:8443',
+        //'node-aliyun-hk-1.etherdream.com:8443'
       ]
     },
     'aliyun-sg': {
       label: '轻量云-新加坡',
       lines: [
-        'node-aliyun-sg.etherdream.com:8443'
+        //'node-aliyun-sg.etherdream.com:8443'
       ]
     },
     'bwh-la': {
       label: '搬瓦工-洛杉矶',
       lines: [
-        'node-bwh-la.etherdream.com:8443'
+        //'node-bwh-la.etherdream.com:8443'
       ]
     },
     'cfworker': {
