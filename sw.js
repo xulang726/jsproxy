ddd3581c5f57401c936f720f0923aad021e6c274
xulang726 <xulang726@users.noreply.github.com>
2019-07-07 20:53:48 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,2 @@
-importScripts(__FILE__='https://cdn.jsdelivr.net/gh/etherdream/jsproxy@gh-pages/assets/bundle.b30fe48b.js')
+//importScripts(__FILE__='https://cdn.jsdelivr.net/gh/etherdream/jsproxy@gh-pages/assets/bundle.b30fe48b.js')
+jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__=x.assets_cdn+'bundle.0668cc6d.js')};importScripts('conf.js')
