33db012fe971c2a597cd0a9a3ef36622a27d092a
xulang726 <xulang726@users.noreply.github.com>
2019-07-08 21:31:42 +0800

    Update conf.js
    name change

--- conf.js
+++ conf.js
@@ -53,7 +53,8 @@ jsproxy_config({
    * 用于加速 `assets` 目录中的资源访问
    */
   //assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
-   assets_cdn: 'https://silent-thunder-4e6a.hellophoto.workers.dev',
+   //assets_cdn: 'https://silent-thunder-4e6a.hellophoto.workers.dev',
+  assets_cdn: 'https://cdn.jsdelivr.net/gh/xulang726/xulang726.github.io@master/assets/',
   // 本地测试时打开，否则访问的是线上的
   // assets_cdn: 'assets/'
 })
